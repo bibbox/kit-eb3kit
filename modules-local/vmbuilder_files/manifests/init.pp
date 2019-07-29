31225fd6a5355ce514728ec1049424e195ba3cfb
reihsr <robert.reihs@gmail.com>
2019-07-29 21:10:40 +0200

    Update init.pp

--- modules-local/vmbuilder_files/manifests/init.pp
+++ modules-local/vmbuilder_files/manifests/init.pp
@@ -183,6 +183,12 @@ class vmbuilder_files(
 
 
 		# Copy bibbox configuration and init scripts
+		file { "/etc/init.d/bibbox":
+				source 		=> '/opt/bibbox/sys-bibbox-vmscripts/initscripts/etc/init.d/bibbox',
+				owner  		=> 'root',
+				group  		=> 'root',
+				mode   		=> '0777'
+		}
 		file { "/etc/init.d/functions":
 				source 		=> '/opt/bibbox/sys-bibbox-vmscripts/initscripts/etc/init.d/functions',
 				owner  		=> 'root',
